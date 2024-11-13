5d27636296bd5cf4a458e888364df082901b9a7c
hoelzflo <florian.hoelz@student.uni-augsburg.de>
2024-11-13 18:38:17 +0100

    Further development of main.py as well as various additions

--- src/Downloader.py
+++ src/Downloader.py
@@ -12,10 +12,10 @@ with open(config_file_path, 'r') as config_file:
 
 def _get_ydl_opts(
         output_dir=None, 
-        subtitle_langs=['en', 'de'], 
-        rate_limit=1000,
-        max_height=1080,
-        download_archive=None
+        subtitle_langs=['en', 'de'],
+        download_archive:str=None, 
+        rate_limit:int=1000,
+        max_height:int=1080
         ) -> dict:
     """
     Generates yt-dlp download preferences
@@ -50,9 +50,6 @@ def _get_ydl_opts(
  
         'ratelimit': rate_limit,                                # Limits download speed
 
-        'download_archive': download_archive,                   # Add downloaded video ids to archive file
-        'break_on_existing': True,                              # Don't download videos with archived ids
-        
         # Download and save metadata and subtitles
         'writeinfojson': True,                                  # Write metadata to .info.json file
         'writethumbnail': True,                                 # Download thumbnail
@@ -68,6 +65,10 @@ def _get_ydl_opts(
             }
         ]
     }
+
+    if download_archive is not None:
+        ydl_opts['download_archive'] = download_archive         # Add downloaded video ids to archive file
+        ydl_opts['break_on_existing'] = True                    # Don't download videos with archived ids
 
     return ydl_opts
 
@@ -89,25 +90,82 @@ def _download_video_by_url(url:str, ydl_opts: dict) -> bool:
 
 
 
-def download(url:str) -> bool:
+def get_video_urls_from_url(url:str) -> list[str]:
+    """
+    Get a list of Video URLs from a Playlist or Channel
+
+    @param url: URL of a playlist or channel (str)
+
+    @Return video_urls: List of video urls (list[str])
+    """
+    # Initialize the yt-dlp options
+    ydl_opts = {
+        'quiet': True,  # Suppress unnecessary output
+        'extract_flat': True,  # Get URLs of videos without downloading
+    }
+    
+    # Create a list to store the URLs
+    video_urls = []
+
+    # Use yt-dlp to extract information
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        # Extract video information from the provided URL (playlist or channel)
+        result = ydl.extract_info(url, download=False)  # Don't download, just extract
+
+        if 'entries' in result:
+            # 'entries' contains the list of videos in the playlist or channel
+            for entry in result['entries']:
+                video_urls.append(entry['url'])  # Add the video URL to the list
+    
+    return video_urls
+
+
+
+def download(
+        url:str, 
+        override_rate_limit:int=None, 
+        override_max_height:int=None) -> bool:
     """
     Main download function. Generates download preferences 
     from config and downloads video using yt-dlp.
 
     @param url: URL of the video to download (string)
+    @param override_rate_limit: Override for the config
+        value of the download rate limit in MB (int)
+    @param override_max_height: Override for the config
+        value of the max video height in pixels (int)
 
-    @return success: Was the download successful (bool)
+    @Return Returns true if download was successful (bool)
     """
+    # Config value for download directory
     active_download_directory = os.path.join(
         config["download_directory_main"],
         config["download_directory_in_progress"],
-        config["download_directory_in_progress_active"]
-    )
+        config["download_directory_in_progress_active"])
+    
+    # Config value for download_archive file
+    download_archive_file = os.path.join(
+        config["download_directory_main"],
+        config["download_directory_data"],
+        config["download_archive_file"])
+    
+    if override_rate_limit is not None:
+        download_rate_limit = override_rate_limit
+    else:
+        download_rate_limit = config["download_rate_limit_in_mb"]
+
+    if override_max_height is not None:
+        max_video_height = override_max_height
+    else:
+        max_video_height = config["download_rate_limit_in_mb"]
 
     ydl_opts = _get_ydl_opts(
-        config["temp_file_directory"],
+        active_download_directory,
         config["subtitle_languages"],
-        config["download_rate_limit_in_mb"])
+        download_archive_file,
+        download_rate_limit,
+        max_video_height
+    )
 
     return _download_video_by_url(url, ydl_opts)
 
--- src/main.py
+++ src/main.py
@@ -2,6 +2,11 @@ import os
 import json
 import logging
 from datetime import datetime
+import argparse
+import sys
+import shutil
+
+import Downloader
 
 # Load config
 config_file_path = 'config/config.json'
@@ -9,40 +14,8 @@ with open(config_file_path, 'r') as config_file:
     config = json.load(config_file)
 
 
-def setup_logger():
-    """
-    Create log-file and logger
-    """
-    # Create a timestamp for the filename
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    log_filename = f"youtube_archiving_log_{timestamp}.txt"
 
-    # Define log-file path
-    log_file = os.path.join(
-        config["download_directory_main"],
-        config["download_directory_data"],
-        config["download_directory_data_logs"],
-        log_filename
-    )
-    
-    # Set up logging configuration
-    logging.basicConfig(
-        level=logging.DEBUG,  # Log level can be changed as needed
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        handlers=[
-            logging.FileHandler(log_file),  # Log to file
-            logging.StreamHandler()             # Optional: Log to console
-        ]
-    )
-    
-    # Create a logger object for other modules to use
-    logger = logging.getLogger(__name__)
-    logger.info(f'Logging started on {timestamp}')
-    return logger
-
-
-
-def check_file_structure() -> None:
+def _check_file_structure() -> None:
     """
     Ensures that the folder structure exists 
     as specified in the config.
@@ -110,44 +83,229 @@ def check_file_structure() -> None:
     if not os.path.exists(download_directory_data_logs):
         os.mkdir(download_directory_data_logs)
 
-    raise Exception("Oh no, something went wrong!")
+    # Check for relevant information files
+    download_archive_file = os.path.join(
+        config["download_directory_main"],
+        config["download_directory_data"],
+        config["download_archive_file"])
+    download_failed_list = os.path.join(
+        config["download_directory_main"],
+        config["download_directory_data"],
+        config["download_failed_list"])
+    download_to_process_list = os.path.join(
+        config["download_directory_main"],
+        config["download_directory_data"],
+        config["download_to_process_list"])
+    if not os.path.exists(download_archive_file):
+        with open(download_archive_file, 'w'):
+            pass
+    if not os.path.exists(download_failed_list):
+        with open(download_failed_list, 'w'):
+            pass
+    if not os.path.exists(download_to_process_list):
+        with open(download_to_process_list, 'w'):
+            pass
+
+
+def _setup_logger(print_to_console:bool=False):
+    """
+    Create log-file and logger
+    """
+    # Create a timestamp for the filename
+    timestamp = datetime.now().strftime("%Y.%m.%d_%H.%M.%S")
+    log_filename = f"youtube_archiving_log_{timestamp}.txt"
+
+    # Define log-file path
+    log_file = os.path.join(
+        config["download_directory_main"],
+        config["download_directory_data"],
+        config["download_directory_data_logs"],
+        log_filename
+    )
+
+    # Define log-file handlers
+    handlers = [logging.FileHandler(log_file)]      # Log to file
+    if print_to_console:
+        handlers.append(logging.StreamHandler())    # Optional: Log to console
+    
+    
+    # Set up logging configuration
+    logging.basicConfig(
+        level=logging.DEBUG,                        # Log level can be changed as needed
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=handlers
+    )
+    
+    # Create a logger object for other modules to use
+    logger = logging.getLogger(__name__)
+    logger.info(f'Logging started on {timestamp}')
+    return logger
+
+
+
+def _parse_arguments():
+    """
+    Parse commandline arguments for use in the script
+    """
+    parser = argparse.ArgumentParser(description="Youtube Archiving")
+
+    # Define arguments for downloading manually specified URL
+    parser.add_argument(
+        "--playlist", action="store_true", required=False,
+        help="FLAG: Download playlist at given URL"
+    )
+    parser.add_argument(
+        "--channel", action="store_true", required=False,
+        help="FLAG: Download channel at given URL"
+    )
+    parser.add_argument(
+        "--url", type=str, required=False, 
+        help="URL to download"
+    )
+
+    # Define argument for downloading URLs from file
+    parser.add_argument(
+        "--file", type=str, required=False, 
+        help="Text file containing URLs to download (one URL per line)"
+    )
+
+    # Define arguments for overriding the config
+    parser.add_argument(
+        "--rate-limit", type=int, required=False, 
+        help="Override the max download rate specified in the config (in MB)"
+    )
+    parser.add_argument(
+        "--max-height", type=int, required=False, 
+        help="Override the max video height specified in the config (in Pixels)"
+    )
+
+    # Parse and return arguments
+    return parser.parse_args()
+
+
+
+def _print_error(message):
+    # ANSI escape code for red text
+    RED = '\033[91m'
+    RESET = '\033[0m'
+    print(f'{RED}[ERROR] {message}{RESET}')
+
+def _print_error_and_exit(
+        message:str, 
+        logger:logging.Logger) -> int:
+    """
+    Prints error to log-file and console, then returns 1
+
+    @param message: Error message (str)
+
+    @Returns 1
+    """
+    logger.error(message)
+    logger.error('Exiting...')
+    _print_error(message)
+    _print_error('Exiting...')
+    return 1
+
 
 
 def main():
-    # Interpret console commands
-    
-
-    
-    
-    
-
     # Create environment
     try:
-        check_file_structure()
+        _check_file_structure()
     except Exception as err:
-        def print_error(message):
-            # ANSI escape code for red text
-            RED = '\033[91m'
-            RESET = '\033[0m'
-            print(f'{RED}[ERROR] {message}{RESET}')
-        
-        print_error('File structure not correct!')
-        print_error(err)
-        print_error('Exiting...')
+        _print_error('File structure not correct!')
+        _print_error(err)
+        _print_error('Exiting...')
         return 1
 
     # Initialize the logger for this script
-    logger = setup_logger()
-        
+    logger = _setup_logger()
     logger.info(f'File structure checked/created successfully.')
 
-    # Download video
+    # Interpret console commands
+    video_urls = []
 
+    if len(sys.argv) == 1:
+        print("No arguments provided. Use --help for assistance.")
+        sys.argv.append("--help")
+    args = _parse_arguments()
+    logger.info('Input parameters:')
+    logger.info(args)
+    
+    if args.file is not None:
+        with open(args.file, 'r', 'utf-8') as url_file:
+            for url in url_file.readlines():
+                video_urls.append(url)
+        if video_urls in [None, []]:
+            return _print_error_and_exit(logger,
+                f'No URLs found in file {args.file}')
+    else: 
+        if args.url is None:
+            return _print_error_and_exit(logger,
+                'Neither URL nor URL-file provided!')
+        
+        if args.playlist or args.channel:
+            video_urls = Downloader.get_video_urls_from_url(args.url)
+            if video_urls in [None, []]:
+                return _print_error_and_exit(logger,
+                    f'No URLs found for '
+                    f'{'playlist' if args.playlist else 'channel'}!')
+        else:
+            video_urls = [args.url]
 
-    # Modify/generate subtitles for downloaded Video
+    logger.info(f'Video URLs to download ({len(video_urls)} total):')
+    logger.info(video_urls)
 
+    # Loop over all videos to download:
+    for i, url in enumerate(video_urls):
+        # Download video
+        logger.info(f'Download {i+1}: {url} with aditional parameters '
+                    f'rate_limit={args.rate_limit} '
+                    f'and max_height={args.max_height}')
+        success = Downloader.download(url, args.rate_limit, args.max_height)
 
-    # Move finalized product to final directories
+        # Check if download was successful
+        if success:
+            logger.info(f'Download {i+1} finished successfully! ({url})')
+        else:
+            # If download unsucessful
+            # Logging
+            logger.error(f'Download {i+1} unsuccessful! ({url})')
+            download_failed_list = os.path.join(
+                config["download_directory_main"],
+                config["download_directory_data"],
+                config["download_failed_list"])
+            with open(download_failed_list, 'a') as failed_list_file:
+                failed_list_file.write(f'{url}\n')
+            logger.info(f'Failed download {i+1}\'s URL added to failed list')
+
+            # Move Files from faild download into the designated directory
+            logger.info(f'Moving failed download {i+1}\'s '
+                        f'files to failed files')
+            download_directory_in_progress_active = os.path.join(
+                config["download_directory_main"],
+                config["download_directory_in_progress"],
+                config["download_directory_in_progress_active"])
+            download_directory_in_progress_failed = os.path.join(
+                config["download_directory_main"],
+                config["download_directory_in_progress"],
+                config["download_directory_in_progress_failed"])
+            failed_files = os.listdir(download_directory_in_progress_active)
+            for file in failed_files:
+                src = os.path.join(download_directory_in_progress_active, file)
+                dst = os.path.join(download_directory_in_progress_failed, file)
+                shutil.move(src, dst)
+            logger.info(f'Finished moving failed download {i+1}\'s '
+                        f'files to failed files')
+        
+        # Modify/generate subtitles for downloaded Video
+
+        # Check whether download contained autogenerated, 
+        # manual or no subtitles
+
+        
+
+        # Move finalized product to final directories
 
 if __name__ == '__main__':
     main()
